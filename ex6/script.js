8b055a2ba83670ca349b8d89f356c6f55997401e
b1329037 <sophiechang0731@gmail.com>
2026-04-08 13:51:54 +0800

    Update script.js

--- ex6/script.js
+++ ex6/script.js
@@ -1,1 +1,22 @@
+var container = document.getElementById('container');
+window.onload = function() {
+    add_new_chars(Math.floor(Math.random() * 3));
+};
 
+window.addEventListener("keyup", function(e) {
+    var currentStr = container.textContent;
+    if (currentStr.length > 0 && e.key === currentStr[0]) {
+        container.textContent = currentStr.substring(1);
+    }
+
+    add_new_chars(Math.floor(Math.random() * 3) + 1);
+});
+
+function add_new_chars(num) {
+    var chars = "abcdefghijklmnopqrstuvwxyz";
+    var result = "";
+    for (var i = 0; i < num; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    container.textContent += result;
+}
